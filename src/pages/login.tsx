aa8a15c9eee533d57664da9d5004466d5aeea5ac
Victor M Militao <victormilitao@gmail.com>
2024-03-05 23:58:15 -0300

    feat: add some input and button style. add login page elements

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,36 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+import { BrowserRouter, Navigate, useNavigate} from 'react-router-dom'
+
 export function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  function handleEmail(event: ChangeEvent<HTMLInputElement>): void {
+    setEmail(event?.target?.value)
+  }
+
+  function handlePassword(event: ChangeEvent<HTMLInputElement>): void {
+    setPassword(event?.target?.value)
+  }
+
+  function handleLogin(event: FormEvent) {
+    event.preventDefault()
+    console.dir(email)
+    console.dir(password)
+    navigate('/')
+  }
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center justify-center gap-12 w-1/3 h-[650px] rounded-[10px] bg-ds-black-500 shadow-[0_0_30px_5px] shadow-ds-black-400">
-        Login
+      <div className="flex flex-col items-center justify-center gap-16 w-1/3 h-[650px] rounded-[10px] bg-ds-black-500 shadow-[0_0_30px_5px] shadow-ds-black-400">
+        <p className="text-[36px]">Investments</p>
+        <form className="flex flex-col gap-4">
+          <input type="text" placeholder="Email" onChange={handleEmail}></input>
+          <input type="password" placeholder="Senha" onChange={handlePassword}></input>
+          <button type="button" className="btn-primary mt-4" onClick={handleLogin}>Login</button>
+        </form>
       </div>
     </div>
   )
 }
-
-// border-radius: 10px;
-// background: var(--ds-black-500);
-// box-shadow: 0px 0px 30px 5px var(--ds-black-400);
