7db3ef8ffebe948f22b9250d11d5436e505646a2
Victor M Militao <victormilitao@gmail.com>
2024-04-03 01:23:58 -0300

    add shadcn lib. init show login error by sonner and message

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,58 +3,53 @@ import { Button } from '../components/button'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import zod from 'zod'
-import { useToast } from '../../src/components/ui/use-toast'
 
 const loginValidation = zod.object({
-  email: zod.string().min(1, 'Informe um email válido'),
-  password: zod
-    .string()
-    .min(8, 'O password deve conter no mínimo 8 caracteres'),
+  name: zod.string().min(1, 'Informe um nome'),
+  email: zod.string().min(1, 'Informe um email'),
+  password: zod.string().min(8, 'Informe um password válido'),
 })
 
-type LoginData = zod.infer<typeof loginValidation>
+type SignupData = zod.infer<typeof loginValidation>
 
-export function Login() {
-  const { toast } = useToast()
+export function Signup() {
   const navigate = useNavigate()
-  const { register, handleSubmit, watch, formState } = useForm<LoginData>({
+  const { register, handleSubmit, watch } = useForm<SignupData>({
     resolver: zodResolver(loginValidation),
     defaultValues: {
+      name: '',
       email: '',
       password: '',
     },
   })
 
+  const name = watch('name')
   const email = watch('email')
   const password = watch('password')
-  const isLoginDisabled = !email || !password
+  const isSignupDisabled = !name || !email || !password
 
-  function handleLogin(data: LoginData): void {
-    toast({
-      title: JSON.stringify(data),
-    })
-    navigate('/')
+  function handleSignup(data: any): void {
+    console.dir(data)
+    navigate('/login')
   }
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div
+      onSubmit={handleSubmit(handleSignup)}
+      className="flex items-center justify-center h-screen"
+    >
       <div className="flex flex-col items-center justify-center gap-16 w-1/3 h-[650px] rounded-[10px] bg-ds-black-500 shadow-[0_0_30px_5px] shadow-ds-black-400">
         <p className="text-[36px]">Investments</p>
-        <form
-          onSubmit={handleSubmit(handleLogin)}
-          className="flex flex-col gap-4 w-1/3"
-        >
+        <form className="flex flex-col gap-4">
+          <input type="text" placeholder="Nome" {...register('name')}></input>
           <input type="text" placeholder="Email" {...register('email')}></input>
           <input
             type="password"
             placeholder="Senha"
             {...register('password')}
           ></input>
-          {formState.errors.password && (
-            <p role="alert">{formState.errors.password.message}</p>
-          )}
-          <Button className="mt-4" disabled={isLoginDisabled}>
-            Login
+          <Button className="mt-4" disabled={isSignupDisabled}>
+            Cadastrar
           </Button>
         </form>
       </div>
