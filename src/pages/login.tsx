23d73591fcd6f7be0bc64fa40b1be5a9878908e2
Victor M Militao <victormilitao@gmail.com>
2024-04-18 00:05:43 -0300

    add some style

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,15 @@
-import { useNavigate } from 'react-router-dom'
-import { Button } from '../components/button'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import zod from 'zod'
-import { useToast } from '../../src/components/ui/use-toast'
+import { useNavigate } from "react-router-dom"
+import { Button } from "../components/button"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import zod from "zod"
+import { useToast } from "../../src/components/ui/use-toast"
 
 const loginValidation = zod.object({
-  email: zod.string().min(1, 'Informe um email válido'),
+  email: zod.string().min(1, "Informe um email válido"),
   password: zod
     .string()
-    .min(8, 'O password deve conter no mínimo 8 caracteres'),
+    .min(8, "O password deve conter no mínimo 8 caracteres"),
 })
 
 type LoginData = zod.infer<typeof loginValidation>
@@ -20,20 +20,20 @@ export function Login() {
   const { register, handleSubmit, watch, formState } = useForm<LoginData>({
     resolver: zodResolver(loginValidation),
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
   })
 
-  const email = watch('email')
-  const password = watch('password')
+  const email = watch("email")
+  const password = watch("password")
   const isLoginDisabled = !email || !password
 
   function handleLogin(data: LoginData): void {
     toast({
       title: JSON.stringify(data),
     })
-    navigate('/')
+    navigate("/")
   }
 
   return (
@@ -42,13 +42,13 @@ export function Login() {
         <p className="text-[36px]">Investments</p>
         <form
           onSubmit={handleSubmit(handleLogin)}
-          className="flex flex-col gap-4 w-1/3"
+          className="flex flex-col gap-4 w-1/3 min-w-max"
         >
-          <input type="text" placeholder="Email" {...register('email')}></input>
+          <input type="text" placeholder="Email" {...register("email")}></input>
           <input
             type="password"
             placeholder="Senha"
-            {...register('password')}
+            {...register("password")}
           ></input>
           {formState.errors.password && (
             <p role="alert">{formState.errors.password.message}</p>
