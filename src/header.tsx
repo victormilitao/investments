dcbc544e3b1c4fa8a74d6f427762560731e9caa3
Victor M Militao <victormilitao@gmail.com>
2024-05-28 17:49:37 -0300

    refactor: change brand spacing

--- src/header.tsx
+++ src/header.tsx
@@ -26,9 +26,11 @@ export function Header() {
   return (
     <header className="min-h-[100px] bg-ds-black-500 px-10 flex items-center">
       <div className="flex items-center gap-10 w-full">
-        <Link to="/" className="text-3xl">
-          Investments
-        </Link>
+        <div className='w-[240px]'>
+          <Link to="/" className="text-3xl">
+            Investments
+          </Link>
+        </div>
         <div className="flex gap-5">
           <Link to="acoes">Ações</Link>
           <span>FII</span>
