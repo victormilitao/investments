77bd07a55ff06570eb6f77c5a347c3a1ec2e14a9
Victor M Militao <victormilitao@gmail.com>
2024-06-16 18:23:32 -0300

    refactor: create color names and fix spacing on stock component

--- src/components/stock/index.tsx
+++ src/components/stock/index.tsx
@@ -14,13 +14,13 @@ export function Stock(props: StockProps) {
 
   return (
     <div className='flex justify-center'>
-    <div className="min-w-[200px] max-w-[200px] flex justify-between gap-3">
-      <img className="rounded-full w-10" src={imgSrc}></img>
+    <div className="min-w-[200px] max-w-[200px] flex">
+      <img className="rounded-full w-10 mr-3" src={imgSrc}></img>
       <div className="flex flex-col">
         <span className="text-sm font-bold">{props.name}</span>
         <span className="text-xs font-normal">{props.ticker_symbol}</span>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col ml-auto">
         <span className="text-sm font-bold text-right">
           {currencyFormatter.format(props.balance)}
         </span>
