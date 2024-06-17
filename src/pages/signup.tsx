63b300319fdb813c3f973a7ff2c85a3dffe699c1
Victor Militão <victormilitao@gmail.com>
2024-06-16 23:22:29 -0300

    refactor: rework on signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,19 +3,23 @@ import { Button } from '../components/button/button'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import zod from 'zod'
+import { useState } from 'react'
 
-const loginValidation = zod.object({
+const signupValidation = zod.object({
   name: zod.string().min(1, 'Informe um nome'),
-  email: zod.string().min(1, 'Informe um email'),
-  password: zod.string().min(8, 'Informe um password válido'),
+  email: zod.string().email('Informe um email válido'),
+  password: zod
+    .string()
+    .min(6, 'O password deve conter no mínimo 6 caracteres'),
 })
 
-type SignupData = zod.infer<typeof loginValidation>
+type SignupData = zod.infer<typeof signupValidation>
 
 export function Signup() {
   const navigate = useNavigate()
-  const { register, handleSubmit, watch } = useForm<SignupData>({
-    resolver: zodResolver(loginValidation),
+  const [isLoading, setIsLoading] = useState(false)
+  const { register, handleSubmit } = useForm<SignupData>({
+    resolver: zodResolver(signupValidation),
     defaultValues: {
       name: '',
       email: '',
@@ -23,24 +27,26 @@ export function Signup() {
     },
   })
 
-  const name = watch('name')
-  const email = watch('email')
-  const password = watch('password')
-  const isSignupDisabled = !name || !email || !password
-
   function handleSignup(data: any): void {
-    console.dir(data)
-    navigate('/login')
+    setIsLoading(true)
+    try {
+      console.dir(data)
+      setTimeout(() => {
+        navigate('/login')
+      }, 3000)
+    } catch (error) {
+      setIsLoading(false)
+    }
   }
 
   return (
-    <div
-      onSubmit={handleSubmit(handleSignup)}
-      className="flex items-center justify-center h-screen"
-    >
+    <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-center gap-16 w-1/3 h-[650px] rounded-[10px] bg-ds-black-500 shadow-[0_0_30px_5px] shadow-ds-black-400">
         <p className="text-[36px]">Investments</p>
-        <form className="flex flex-col gap-4">
+        <form
+          className="w-1/2 flex flex-col gap-4"
+          onSubmit={handleSubmit(handleSignup)}
+        >
           <input type="text" placeholder="Nome" {...register('name')}></input>
           <input type="text" placeholder="Email" {...register('email')}></input>
           <input
@@ -48,7 +54,7 @@ export function Signup() {
             placeholder="Senha"
             {...register('password')}
           ></input>
-          <Button className="mt-4" disabled={isSignupDisabled}>
+          <Button className="mt-4" loading={isLoading}>
             Cadastrar
           </Button>
         </form>
